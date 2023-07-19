862a4dbc4ae55e24c2d54a21b330622be8bf3a37
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-07-19 03:16:50 +0200

    Resolve conflicts with app.modules.ts and app.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
 
 }
